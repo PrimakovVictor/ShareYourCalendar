99f1613d750f10e72b46176b29953d992ed30873
Victor Primakov <victorprimakov@MacBook-Pro-Victor.local>
2025-04-09 01:40:25 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MonthCalendar from "./components/MonthCalendar";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <MonthCalendar />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mint: {
+          100: "#C8F7DC"
+        },
+        peach: {
+          200: "#FFDAB9"
+        }
+      }
+    },
+  },
+  plugins: [],
+};
